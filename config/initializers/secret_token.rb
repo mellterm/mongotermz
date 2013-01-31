7643c0b8b69d6e6ec748d8c4ff3c0942bcee4b5a
Vince The Mac Vincent <mellterm@gmail.com>
2013-02-01 04:15:57 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongotermz::Application.config.secret_token = '1be622cee14399b00494ddc0c9bd6af62993441c19ba3e4c1595428d122f2829904c4bf7543a3db261a5c892cb5cfced4a8799efdab9a8641921c8141b0ef230'
